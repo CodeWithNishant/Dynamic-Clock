ed9dbeeec5002cbeaa0fbaf229005b48e9bd302f
Nishant Nagar <nishantnagar54321@gmail.com>
2024-07-10 18:03:36 +0530

    update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="track" element={<TrackingScreen />} />
         <Route path="/" element={<Onboarding />} />
+        <Route path="/Dynamic-Clock" element={<Onboarding />} />
       </Routes>
     </Router>
   );
